0cb7e9f5791cb025f7c0a5dbd31fad5a8012d58e
nicolas <nicolas110996@example.com>
2021-02-11 21:41:39 -0300

    Adjust code

--- tasky_app/input_output/task_file_repository.py
+++ tasky_app/input_output/task_file_repository.py
@@ -2,11 +2,15 @@ from typing import List
 
 from tasky_app.domain.task_repository import TaskRepository
 from tasky_app.domain.task import Task
+from tasky_app.domain.exceptions import TaskNotFound
 
 
 class TaskFileRepository(TaskRepository):
 
-    SOURCE_PATH = "~/.tasky/tasks"
+    SOURCE_PATH = ""
+
+    def __init__(self, source_path: str):
+        self.SOURCE_PATH = source_path or "tasky_app/db/tasks_db"
 
     def _to_task(self, line: str) -> Task:
         name = ""
@@ -20,13 +24,13 @@ class TaskFileRepository(TaskRepository):
             pass
 
     def find_by_id(self, id: int) -> Task:
-        with open(self.SOURCE_PATH, "w") as file:
+        with open(self.SOURCE_PATH) as file:
             for line in file:
                 if id in line:
                     return line
             else:
-                raise Exception
+                raise TaskNotFound
 
     def find_all(self) -> List[Task]:
-        with open(self.SOURCE_PATH, "w") as file:
+        with open(self.SOURCE_PATH) as file:
             return [self._to_task(line) for line in file]
