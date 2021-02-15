9b8035a67a1a8b374e739dc585a34cae233dfaf0
nicolas <nicolas110996@example.com>
2021-02-15 01:55:07 -0300

    Add task creator entrypoint

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from tasky_app.cli.tasky_entrypoint import parser
 from tasky_app.input_output.task_file_repository import TaskFileRepository
 from tasky_app.domain.task import Task
 from tasky_app.services.task_finder import TasksFinder
+from tasky_app.services.task_creator import TaskCreator
 
 
 def display_task(task: Task):
-    icon = "✔️" if task.is_done else "❌"
+    icon = "✔️" if task.is_done else "⭕"
     print("{}_  {}: {} -- {}".format(
         task.id,
         task.name,
@@ -22,3 +23,7 @@ if __name__ == "__main__":
         tasks = TasksFinder(TaskFileRepository()).find_all()
         for task in tasks:
             display_task(task)
+    elif arguments.add:
+        creator = TaskCreator(TaskFileRepository())
+        creator.create(arguments.add)
+        print("Task created 📋🖊️")
--- tasky_app/cli/tasky_entrypoint.py
+++ tasky_app/cli/tasky_entrypoint.py
@@ -6,9 +6,3 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--add", help="Add a new task", type=str)
 parser.add_argument("-l", "--list", help="List all tasks", action="store_true")
 parser.add_argument("-s", "--select", help="Select one task", type=int)
-
-# args = parser.parse_args()
-
-# print(args.add)
-# print(args.list)
-# print(args.select)
